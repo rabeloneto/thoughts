6e0c302df2b05ce5c204ea88c32f7052e85003d8
Paulo <paulorabelonetoo@gmail.com>
2025-07-08 21:23:40 -0300

    finalizando

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -6,6 +6,39 @@ module.exports = class AuthController {
     static login(req,res){
         res.render('auth/login')
     }
+    static async loginPost(req,res){
+
+        const {email,password} = req.body
+
+        //find user 
+        const user = await User.findOne({where: {email:email}})
+
+        if(!user){
+            req.flash('message','Usuario nao encontrado')
+            res.render('auth/login')
+            return
+        }
+
+        //check if password match
+        const passwordMatch = bcrypt.compareSync(password, user.password)
+
+        if(!passwordMatch){
+            req.flash('message','Senha invalida')
+            res.render('auth/login')
+            return
+        }
+
+        //initialize session
+          req.session.userid = user.id  
+
+          req.flash('message','autentificação realizado com sucesso!')
+
+          req.session.save(() => {
+            res.redirect('/')
+          })
+
+
+    }
     static register(req,res){
         res.render('auth/register')
     }
@@ -21,6 +54,47 @@ module.exports = class AuthController {
 
             return 
         }
+        //check if user exists
+        const checkIfUserExists = await User.findOne({where: {email:email}})
 
+        if(checkIfUserExists){
+            req.flash('message',"O email já esta em uso!")
+            res.render('auth/register')
+
+            return 
+        }
+
+        //create a password
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(password, salt);
+
+        const user = {
+            name,
+            email,
+            password: hashedPassword
+        };
+
+        try{
+          const createdUser = await User.create(user)
+
+          //initialize session
+          req.session.userid = createdUser.id  
+
+          req.flash('message','Cadastro realizado com sucesso!')
+
+          req.session.save(() => {
+            res.redirect('/')
+          })
+          res.redirect('/')
+        }catch(err){
+
+            console.log(err)
+        }
+
+
+    }
+    static logout(req,res){
+        req.session.destroy()
+        res.redirect('/login')
     }
 }
---
+++ helpers/auth.js
@@ -0,0 +1,10 @@
+module.exports.checkAuth = function(req,res,next){
+
+    const userid = req.session.userid
+
+    if(!userid){
+        res.redirect('/login')
+    }
+
+    next()
+}
